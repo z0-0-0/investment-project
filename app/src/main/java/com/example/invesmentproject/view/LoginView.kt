3853dcc221dd33253c41e694c7fbfe5294d4da83
redvillage <338990@niuitmo.ru>
2023-12-12 18:22:15 +0300

    Login/Onboarding View

--- app/src/main/java/com/example/invesmentproject/view/LoginView.kt
+++ app/src/main/java/com/example/invesmentproject/view/LoginView.kt
@@ -1,15 +1,78 @@
 package com.example.invesmentproject.view
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
-import com.example.invesmentproject.viewmodel.LoginViewViewModel
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.invesmentproject.ui.theme.InvesmentAppTheme
 
+@Preview
 @Composable
 fun LoginView(
-    navController: NavController,
-    viewModel: LoginViewViewModel = hiltViewModel()
+    //  navController: NavController,
+    // viewModel: LoginViewViewModel = hiltViewModel()
 ) {
-    Text("Login")
+    InvesmentAppTheme {
+        Surface(
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    LoginCard()
+                }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun LoginCard() {
+    InvesmentAppTheme {
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            ),
+            modifier = Modifier
+                .fillMaxWidth(0.8F)
+                .fillMaxHeight(0.7F),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor =  MaterialTheme.colorScheme.primary
+            )
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    text = "Login Screen"
+                )
+               // Spacer(modifier = Modifier.size(20.dp))
+                TextField(value = "Login", onValueChange = {/*TODO*/})
+                TextField(value = "Password", onValueChange = {/*TODO*/})
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Go")
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/invesmentproject/view/OnboardingView.kt
+++ app/src/main/java/com/example/invesmentproject/view/OnboardingView.kt
@@ -1,15 +1,18 @@
 package com.example.invesmentproject.view
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -29,12 +32,15 @@ fun OnboardingView(
     HorizontalPager(state = pagerState) { page ->
         InvesmentAppTheme {
             Surface(
-                Modifier.fillMaxSize()
+                Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
             ) {
                 Column(
                     Modifier
                         .systemBarsPadding()
-                        .padding(horizontal = 16.dp)
+                        .padding(horizontal = 16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                 //    Image(painter = , contentDescription = )
                     Text("test")
